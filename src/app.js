172a7848d266508edb0a7aafed35c54bcb4e7b28
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2024-11-25 15:54:10 +0530

    sending dynamic data using signup api

--- src/app.js
+++ src/app.js
@@ -1,114 +1,88 @@
 
 
-const express=require("express")
-const app=express();
-const PORT=3000;
-// app.use("/ab?c",(req,res)=>{
-//     res.send("b is optional")
-// })
-// app.use("/ab+c",(req,res)=>{
-//     res.send("b can be howmuch")
-// })
+// const express=require("express")
+// const app=express();
+// const PORT=3000;
+// const {connectDB}=require("./config/database")
+// const User=require("./models/user")
 
-// app.use("/ab*c",(req,res)=>{
-//     res.send("anthing can be between ab and c")
-// })
-
-
-// app.use("/a(bc)?d",(req,res)=>{
-//     res.send("bc is optional")
+// app.post("/signup",async(req,res)=>{
+// //creating a new instance of a model User
+// const user=new User({
+//   firstName:"sushil",
+//   lastName:"kushwah",
+//   emailId:"sushilkushwah@gmail.com",
+//   password:"Sushil@1411",
+//   gender:"male",
 // })
 
-// app.get("/user",(req,res)=>{
-//     console.log(req.query);//for query at api request time
-//     res.send({firstName:"Sushil",LastName:"Kushwah"})
-//     })
-//     app.get("/user/:userId/:name/:loc",(req,res)=>{
-//         console.log(req.params);//for params at api dynamic request time
-//         res.send("user id direct passing and dynamic routing")
-        
-//     })
+// await user.save();
+// res.send("user collection data is saved successfully")
+// })
 
-
-// app.get("/user",(req,res)=>{
-//     console.log(req.query);
-//     res.send({firstName:"Sushil",LastName:"Kushwah"})
+// connectDB()
+// .then(()=>{
+//     console.log("Database connection established...");
+//     app.listen(PORT,()=>{
+//       console.log("Server running successfully on 3000..."); 
+//   })//we are listening the request.
     
 // })
-
-
-// app.get("/33/23",(req,res)=>{
-//     res.send("this is by get method")
+// .catch((err)=>{
+//    console.error("Database cannot be connect!!")
 // })
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const { connectDB } = require("./config/database");
+const User = require("./models/user");
 
-// app.get("/user",(req,res)=>{
-//     res.send({firstName:"Sushil",LastName:"Kushwah"})
+// Middleware to parse JSON request body
+app.use(express.json());
+
+// app.post("/signup", async (req, res) => {
+//   try {
+//     const user = new User({
+//       firstName: "Harikrishna",
+//       lastName: "jaiiswal",
+//       emailId: "hari@gmail.com",
+//       password: "hariiii21005",
+//       gender: "male",
+//     });
+
+//     await user.save();
+//     res.send("User collection data is saved successfully.");
+//   } catch (error) {
+//     console.error("Error saving user data:", error.message);
+//     res.status(500).send("Failed to save user data.");
+//   }
+// });
+
+app.post("/signup",async(req,res)=>{
+
+    const user=new User(req.body)
+    try{
+await user.save();
+res.send("data is saved in User model collection")
+  }catch(error){
+      console.log("error in saving data",error.message);
+      res.status(500).send("failed to store the data")
+      
+  }
+})
+
+// Connect to the database and start the server after connection
+connectDB().then(() => {
+  console.log("Database connection established successfully...");
+  app.listen(PORT, () => {
+    console.log(`Server running successfully on port ${PORT}...`);
+  });
+});
+// connectDB().then(()=>{
+//   console.log("established");
+//   app.listen(PORT,()=>{
+//     console.log(`server running on ${PORT}`);
+    
+//   })
+  
 // })
-
-// app.post("/user",(req,res)=>{
-//     //saving data to the database
-//     res.send("Data successfully saved to the database")
-// })
-
-// app.use("/user",(req,res)=>{
-//     res.send("HAHAHAHAH")
-// })
-
-// app.delete("/user",(req,res)=>{
-//     //To delete data form database
-//     res.send("data deleted successfully")
-// })
-
-// app.use("/",(req,res)=>{
-//     res.send("only by / ")
-// })//it is by default type of rout,after"/" anything then this will be executed
-// app.use("/hello",(req,res)=>{
-//     res.send("hello hello.")
-// })
-
-
-//****Handling the routers****//
-// app.use("/routers",[r1,r2,r3,r4,r5])
-// res.send("1st response") are called request handlers
-//("/user",[(req,res,next)=>{
-    // res.send("1st response")
-    //} this whole is called middlewares
-
-
-//***handle Auth Middleware for only all GET,Post,Patch.. request ***/
-    // const {adminauth}=require("./middlewares/auth")
-    // const {usernauth,Admin}=require("./middlewares/auth")
-
-
-    // app.use("/admin",adminauth);
-
-    // app.post("/admin/login",(req,res)=>{
-    //   res.send(" user logged in successfully")
-    // })
-        
-        // Route to get all admin data
-        // app.get("/admin/getAllData", usernauth,(req, res) => {
-        //   res.send("All data is sent");
-        // });
-        
-        // Route to delete a user
-        // app.get("/admin/deleteUser", Admin,(req, res) => {
-        //   res.send("User data deleted");
-        // });
-      app.get("/getUserData", (req, res) => {
-         //logic of Db callling and get userdata
-       
-         throw new Error("fkhahlkeh")   
-       
-         res.send("user data is sent");
-        });
-        app.use("/",(err,req,res,next)=>{
-              if(err){
-                res.status(500).send("something went wrong")
-              }
-        })
-
-
-app.listen(PORT,()=>{
-    console.log("Server running successfully on 3000..."); 
-})//we are listening the request.
---
+++ src/config/database.js
@@ -0,0 +1,46 @@
+// const mongoose=require("mongoose")
+
+// const connectDB=async()=>{
+//     await mongoose.connect(
+//         "mongodb+srv://kartikkushwah1411:Sushilkushwah@sushilnode.gxkj2.mongodb.net/devTinder"
+//     ,{
+//         useNewUrlParser:true,
+//         useUnifiedTopology:true,
+//         keepAlive:true,
+//         keepAliveInitialDelay:300000,
+
+//     }
+// );
+// }
+// module.exports={connectDB}
+
+const mongoose = require("mongoose");//mongoose functions always returns a PROMISE so we use async and await
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://kartikkushwah1411:Sushilkushwah@sushilnode.gxkj2.mongodb.net/devTinder"
+    );
+    // console.log("Database connection established successfully...");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1); // Exit the process if the connection fails
+  }
+};
+
+module.exports = { connectDB };
+
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://kartikkushwah1411:Sushilkushwah@sushilnode.gxkj2.mongodb.net/devTinder"
+//     );
+//     console.log("Database is connected with mongoose successfully");
+//   } catch (error) {
+//     console.error("connection failed",error.message)
+//   }
+// };
+
+// module.exports=connectDB;
