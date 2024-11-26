5893849378cf1dc11e120d109140726639f2b4c6
Sushilkushwah14 <sushilkushwah1411@gmail.com>
2024-11-26 17:32:53 +0530

    Data sanitizing and schema validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-
-
 // const express=require("express")
 // const app=express();
 // const PORT=3000;
@@ -24,9 +22,9 @@
 // .then(()=>{
 //     console.log("Database connection established...");
 //     app.listen(PORT,()=>{
-//       console.log("Server running successfully on 3000..."); 
+//       console.log("Server running successfully on 3000...");
 //   })//we are listening the request.
-    
+
 // })
 // .catch((err)=>{
 //    console.error("Database cannot be connect!!")
@@ -36,9 +34,7 @@ const app = express();
 const PORT = 3000;
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
-
-// Middleware to parse JSON request body
-app.use(express.json());
+const user = require("./models/user");
 
 // app.post("/signup", async (req, res) => {
 //   try {
@@ -58,31 +54,101 @@ app.use(express.json());
 //   }
 // });
 
-app.post("/signup",async(req,res)=>{
+// Middleware to parse JSON request body
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body); //reading the data
+  console.log(user);
 
-    const user=new User(req.body)
-    try{
-await user.save();
-res.send("data is saved in User model collection")
-  }catch(error){
-      console.log("error in saving data",error.message);
-      res.status(500).send("failed to store the data")
-      
+  try {
+    await user.save();
+    res.send("data is saved in User model collection");
+  } catch (error) {
+    console.log("error in saving data", error.message);
+    res.status(500).send("failed to store the data");
   }
-})
+});
+app.get("/users", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("users not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("something weent wrong");
+  }
+});
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("something weent wrong ,wrong email");
+  }
+});
+//to delete in database
+app.delete("/user", async (req, res) => {
+  const userid = req.body._id;
+  try {
+    await User.findByIdAndDelete({ userid });
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("something weent wrong");
+  }
+});
+//to update data of the user
+app.patch("/user", async (req, res) => {
+  const userid = req.body._id;
+  const data = req.body;
+  try {
+    // const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    // const isUpdateAllowed = Object.keys(data).every((k) =>
+    //   ALLOWED_UPDATES.includes(k)
+    // );
+    // if (!isUpdateAllowed) {
+    //   throw new Error("Update is not allowed");
+    // }
+    if(data?.skills?.length>3){
+      throw new Error("skills are too many ")
+    }
+    //id wale main,all "data" ke andar kuch bhi update kar sakte hai
+    await User.findByIdAndUpdate({ _id: userid }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("UPDATE failed" + error.message);
+  }
+});
+//to update data of the user in email
+app.patch("/user", async (req, res) => {
+  const userid = req.body._id;
+  const updateEmail = req.body.emailId;
+  try {
+    //emailid wale kitnebhi main,all "emailId:updateEmail" ke andar kuch bhi update kar sakte hai
+    await User.findByIdAndUpdate(
+      { userid },
+      { emailId: updateEmail },
+      {
+        returnDocument: "after",
+        runValidators: true,
+      }
+    );
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("UPDATE failed" + error.message);
+  }
+});
 
 // Connect to the database and start the server after connection
 connectDB().then(() => {
   console.log("Database connection established successfully...");
   app.listen(PORT, () => {
-    console.log(`Server running successfully on port ${PORT}...`);
+    console.log(`Server running successfuslly on port ${PORT}...`);
   });
 });
-// connectDB().then(()=>{
-//   console.log("established");
-//   app.listen(PORT,()=>{
-//     console.log(`server running on ${PORT}`);
-    
-//   })
-  
-// })
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,68 @@
 const mongoose = require("mongoose");
-
+const validator=require("validator")
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
+    maxLength:50,
   },
   lastName: {
     type: String,
+    maxLength:50,
   },
   emailId: {
     type: String,
+    require:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("invalid email address :" +value)
+      }
+    }
   },
   password: {
     type: String,
+    required:true,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("invalid password :"+value)
+      }
+    }
   },
   age: {
     type: Number,
+    min:18,
+
   },
   gender: {
     type: String,
+     validate(value){
+     if(!["male","female","others"].includes(value)){
+      throw new Error("Gender data is not valid")
+     }
+    },
   },
-});
+  phototUrl:{
+    type:String,
+    default:"https://photo.url.defaulturl.com",
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("invalid photo url :"+value)
+      }
+    }
+  },
+  about:{
+    type:String,
+    default:"this is a default description",
+  },
+  skills:{
+    type:[String],
+  }
+},
+{
+timestamps:true,
+}
+);
 module.exports = mongoose.model("User", userSchema); //model name and schema
